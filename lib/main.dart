9dc01738b72f9eae55c2bebbfe5e539223bc978e
KucingBobok <afiksinggih@gmail.com>
2023-05-19 20:37:53 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.download),
+          ),
+          body: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(children: [
+                const TextField(
+                    decoration: InputDecoration(labelText: ("Username"))),
+                const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: ("Password"))),
+                const SizedBox(height: 10.0),
+                ElevatedButton(
+                    onPressed: () {
+                      Get.snackbar('Hi', 'Message');
+                    },
+                    child: const Text("Login"))
+              ])),
+          appBar: AppBar(
+            title: const Text("Aplikasi Widget"),
+          )),
+    ),
+  );
+}
